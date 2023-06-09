5d0f880a3855d9f9406957837c40a19857d445ae
Artemy Gibson <artemygibson@gmail.com>
2023-06-09 16:14:49 -0700

    refactor: Refactor initialize block

--- lib/season.rb
+++ lib/season.rb
@@ -10,63 +10,67 @@ class Season
               :team_tackles
 
   def initialize(game_file, game_team_file, season, team_data)
-    @games = []
-    @game_teams = []
     @season = season
-    @game_ids = []
-    @teams = []
-    @team_ids = []
-    @team_tackles = {}
-    generate_games(game_file)
-    generate_game_ids
-    generate_game_teams(game_team_file)
-    generate_teams(team_data)
-    generate_team_ids
-    tackle_data
+    @games = generate_games(game_file)
+    @game_ids = generate_game_ids
+    @game_teams = generate_game_teams(game_team_file)
+    @team_ids = generate_team_ids
+    @teams = generate_teams(team_data)
+    @team_tackles = tackle_data
   end
 
   def generate_games(game_file)
+    games = []
     game_lines = CSV.open game_file, headers: true, header_converters: :symbol
     game_lines.each do |line|
-        if line[:season] == @season
-            @games << Game.new(line)
-        end
+      if line[:season] == @season
+        games << Game.new(line)
+      end
     end
+    @games = games
   end
 
   def generate_game_ids
+    game_ids = []
     @games.each do |game|
-        @game_ids << game.game_id
+      game_ids << game.game_id
     end
+    @game_ids = game_ids
   end
   
   def generate_game_teams(game_team_file)
+    game_teams = []
     game_lines = CSV.open game_team_file, headers: true, header_converters: :symbol
     game_lines.each do |line|
-        if @game_ids.include?(line[:game_id])
-            @game_teams << GameTeam.new(line)
-        end
+      if @game_ids.include?(line[:game_id])
+        game_teams << GameTeam.new(line)
+      end
     end
+    @game_teams = game_teams
   end
       
   def generate_team_ids
+    team_ids = []
     @games.each do |game|
-        if !@team_ids.include?(game.away_team_id)
-            @team_ids << game.away_team_id
+        if !team_ids.include?(game.away_team_id)
+            team_ids << game.away_team_id
         end
-        if !@team_ids.include?(game.home_team_id)
-            @team_ids << game.home_team_id
+        if !team_ids.include?(game.home_team_id)
+            team_ids << game.home_team_id
         end
     end
+    @team_ids = team_ids
   end
 
   def generate_teams(team_data)
+    teams = []
     game_lines = CSV.open team_data, headers: true, header_converters: :symbol
     game_lines.each do |line|
         if !@team_ids.include?(line[:team_id])
-            @teams << Team.new(line)
+            teams << Team.new(line)
         end
     end
+    @teams = teams
   end
 
   def games_count
