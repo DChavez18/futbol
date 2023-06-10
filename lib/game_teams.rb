2bf02c5aa75f1d66d9227f2e20b1a2f729f9cfeb
Garrett Varenhorst <garrettvarenhorst@gmail.com>
2023-06-10 11:02:10 -0500

    fix: Correct indentation

--- lib/game_teams.rb
+++ lib/game_teams.rb
@@ -1,25 +1,25 @@
 require "csv"
 
 class GameTeam
-    attr_reader :team_id,
-                :home_or_away,
-                :result,
-                :head_coach,
-                :goals,
-                :shots,
-                :tackles
+  attr_reader :team_id,
+              :home_or_away,
+              :result,
+              :head_coach,
+              :goals,
+              :shots,
+              :tackles
 
-    def initialize(game_team_data)
-        @team_id = game_team_data[:team_id]
-        @home_or_away = game_team_data[:hoa]
-        @result = game_team_data[:result]
-        @head_coach = game_team_data[:head_coach]
-        @goals = game_team_data[:goals].to_i
-        @shots = game_team_data[:shots].to_i
-        @tackles = game_team_data[:tackles].to_i
-    end
+  def initialize(game_team_data)
+    @team_id = game_team_data[:team_id]
+    @home_or_away = game_team_data[:hoa]
+    @result = game_team_data[:result]
+    @head_coach = game_team_data[:head_coach]
+    @goals = game_team_data[:goals].to_i
+    @shots = game_team_data[:shots].to_i
+    @tackles = game_team_data[:tackles].to_i
+  end
 
-    def accuracy
-        @goals.to_f / @shots
-    end
+  def accuracy
+    @goals.to_f / @shots
+  end
 end
